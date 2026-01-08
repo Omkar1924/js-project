5ef8fa83ff6338c1e6a49e7472f8819d393972cc
Omkar1924 <151366943+Omkar1924@users.noreply.github.com>
2026-01-08 10:24:49 +0000

    rename

---
+++ 01.basics/02-datatype.js
@@ -0,0 +1,16 @@
+//datatype in js
+//premative data type
+let name="omker" // string
+let age=23  //number => 2^53
+let login=true  // boolen
+let num  // undefine
+let temp = null // representation of empty value
+
+//nonpremative data type
+Object
+
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof login)
+console.log(typeof temp)
